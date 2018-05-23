940a4af10f46abe7b83aca301e594385c7436474
Adolfo <adolfo@unizar.es>
2018-05-24 00:30:14 +0200

    Different functions.

--- main/doc/simple-function/simple-function.cc
+++ main/doc/simple-function/simple-function.cc
@@ -1,8 +1,17 @@
 #include <fun.h>
 #include <iostream>
 
-
+double sqr(double d) { return d*d; }
+struct Sqr {
+    double operator()(double d) const { return d*d; }	
+};
 int main(int argc, char** argv) {
-	auto my_function = fun::function<double, double>([] (double f) { return f*f; });
-	std::cout<<my_function(2)<<std::endl;
+	auto fun_sqr1 = fun::function<double, double>([] (double f) { return f*f; });
+//	auto fun_sqr2 = fun::function<double, double>(sqr);
+	auto fun_sqr3 = fun::function<double, double>(Sqr());
+
+
+	std::cout<<fun_sqr1(2)<<" "<<
+		       //fun_sqr2(2)<<" "<<
+			   fun_sqr3(2)<<std::endl;
 }
