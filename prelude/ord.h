24510b19177508aabb0693d7e0bace204ee09b7f
Adolfo <adolfo@unizar.es>
2018-07-01 11:10:53 +0200

    Solved bug in Eq

--- prelude/ord.h
+++ prelude/ord.h
@@ -11,6 +11,7 @@ struct Ord {
 	struct check {
 		template<typename U>
 	    static auto test(U*) -> typename std::is_same<decltype(std::declval<U>() <= std::declval<U>()), bool>::type;
+		
         template<typename>
         static constexpr std::false_type test(...);
 
